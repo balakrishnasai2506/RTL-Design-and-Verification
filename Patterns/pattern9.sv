c55e5a8d2d85814b97809cd097228c28333ce8aa
Bala Krishna Sai <brucewayne25062000@gmail.com>
2025-10-04 12:49:52 +0530

    Create pattern9.sv

---
+++ Patterns/pattern9.sv
@@ -0,0 +1,23 @@
+//Generate a constraint such that it gives a pattern 1 22 333 4444 55555......
+class pattern_gen;
+  rand int num;
+  constraint num_size{num == 10;}
+  function void post_randomize();
+    for(int i = 0; i <= 10; i++) begin
+      for(int j = 0; j <= i; j++) begin
+        $write("%0d", i);
+      end
+      $write("\t");
+    end
+  endfunction
+endclass
+
+pattern_gen pgen;
+
+module test;
+  initial begin
+    pgen = new;
+    pgen.randomize();
+    $display("%p", pgen);
+  end
+endmodule
