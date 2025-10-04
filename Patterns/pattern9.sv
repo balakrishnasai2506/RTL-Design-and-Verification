fb7f97a44d1c8b597fd972954ac59403a38aff83
Bala Krishna Sai <brucewayne25062000@gmail.com>
2025-10-04 12:50:51 +0530

    Update pattern9.sv

--- Patterns/pattern9.sv
+++ Patterns/pattern9.sv
@@ -3,7 +3,7 @@ class pattern_gen;
   rand int num;
   constraint num_size{num == 10;}
   function void post_randomize();
-    for(int i = 0; i <= 10; i++) begin
+    for(int i = 0; i < 10; i++) begin
       for(int j = 0; j <= i; j++) begin
         $write("%0d", i + 1);
       end
