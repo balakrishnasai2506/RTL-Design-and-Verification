9c1adf3bdbbf92951452eba8118bc1b578fda57f
Bala Krishna Sai <brucewayne25062000@gmail.com>
2025-10-05 08:46:00 +0530

    Create test10.sv

--- Constraints and Arrays/test10.sv
+++ Constraints and Arrays/test10.sv
@@ -1,7 +1,6 @@
 //  Triangle pattern: Constrain random variables to form the printed pattern: 1, 22, 333, 4444, 55555 (as rows).
 class base;
   rand int num;
-  int a[];
   constraint num_val{ num == 10;}
   function void post_randomize();
     for(int i = 0; i < num; i++)  begin
